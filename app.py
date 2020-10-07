5247b36801c6f8fd5acdcc592a7da1f5821b458b
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2020-10-07 11:36:51 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, redirect
+from flask_ngrok import run_with_ngrok
+import pickle
+
+#Create the flask object
+app = Flask(__name__)
+run_with_ngrok(app)
+
+#create route
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+   if request.method=='POST':
+      message=request.form['message']
+      output=generate_summary(message, 3)
+      return render_template('index.html', summarized_text=output)
+   else:
+      return redirect('/')
+
+if __name__=="__main__":
+   app.run()
