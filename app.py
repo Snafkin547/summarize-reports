b92715ccb409b114a0e168849d26c6f81d659923
Harunobu Ishii <harunobuishii547@gmail.com>
2020-10-07 15:15:37 +0800

    summarize function

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, request, jsonify, render_template, redirect
 from flask_ngrok import run_with_ngrok
 import pickle
 
+from summarize import generate_summary
+
 #Create the flask object
 app = Flask(__name__)
 run_with_ngrok(app)
